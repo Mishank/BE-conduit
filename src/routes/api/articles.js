5c4ecdd911e28ec947efbebd72b619a6c04d937b
Misha <Misha140699@mail.ru>
2023-05-02 16:35:19 +0300

    exposing a global tags

--- src/routes/api/articles.js
+++ src/routes/api/articles.js
@@ -208,4 +208,13 @@ router.delete(
   }
 );
 
+router.get("/", function (req, res, next) {
+  Article.find()
+    .distinct("tagList")
+    .then(function (tags) {
+      return res.json({ tags: tags });
+    })
+    .catch(next);
+});
+
 module.exports = router;
