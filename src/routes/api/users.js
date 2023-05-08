931a31f5d3e31a05eb3b4dc4b52fe3cfa672be0a
Misha <Misha140699@mail.ru>
2023-05-08 13:56:08 +0300

    add refactor Update user work

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -4,7 +4,12 @@ var passport = require("passport");
 
 var User = mongoose.model("User");
 var auth = require("../auth");
-const { register, login, user } = require("../../services/user.service");
+const {
+  register,
+  login,
+  user,
+  userput,
+} = require("../../services/user.service");
 
 router.post("/users", function (req, res, next) {
   const userReq = req.body.user;
@@ -26,33 +31,8 @@ router.get("/user", auth.required, function (req, res, next) {
 });
 
 router.put("/user", auth.required, function (req, res, next) {
-  User.findById(req.payload.id)
-    .then(function (user) {
-      if (!user) {
-        return res.sendStatus(401);
-      }
+  const userReq = req.body.user;
 
-      // only update fields that were actually passed...
-      if (typeof req.body.user.username !== "undefined") {
-        user.username = req.body.user.username;
-      }
-      if (typeof req.body.user.email !== "undefined") {
-        user.email = req.body.user.email;
-      }
-      if (typeof req.body.user.bio !== "undefined") {
-        user.bio = req.body.user.bio;
-      }
-      if (typeof req.body.user.image !== "undefined") {
-        user.image = req.body.user.image;
-      }
-      if (typeof req.body.user.password !== "undefined") {
-        user.setPassword(req.body.user.password);
-      }
-
-      return user.save().then(function () {
-        return res.json({ user: user.toAuthJSON() });
-      });
-    })
-    .catch(next);
+  userput(userReq, req, res, next);
 });
 module.exports = router;
