11b984ac0f01fd7b07af433b7da03f37b75a84b9
Sergey Komarov <komarovs33@mail.ru>
2023-05-10 10:15:36 +0300

    fix: Delete import

--- src/services/article.service.js
+++ src/services/article.service.js
@@ -3,7 +3,6 @@ var passport = require("passport");
 var mongoose = require("mongoose");
 var Article = mongoose.model("Article");
 var User = mongoose.model("User");
-var auth = require("../auth");
 
 function article(userReq, req, res, next) {
   User.findById(req.payload.id)
