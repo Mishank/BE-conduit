c77c04a15491fad3e28e44fa711531f0db9d46c3
Misha <Misha140699@mail.ru>
2023-05-10 12:37:19 +0300

    refactor articleGet

--- src/routes/api/articles.js
+++ src/routes/api/articles.js
@@ -10,6 +10,7 @@ const {
   articleFeed,
   articleParam,
   articlePut,
+  articleGet,
 } = require("../../services/article.service");
 
 router.param("article", function (req, res, next, slug) {
@@ -168,64 +169,10 @@ router.delete(
 );
 
 router.get("/", auth.optional, function (req, res, next) {
-  var query = {};
-  var limit = 20;
-  var offset = 0;
+  //refactor
+  const userReq = req.body.user;
 
-  if (typeof req.query.limit !== "undefined") {
-    limit = req.query.limit;
-  }
-
-  if (typeof req.query.offset !== "undefined") {
-    offset = req.query.offset;
-  }
-  if (typeof req.query.tag !== "undefined") {
-    query.tagList = { $in: [req.query.tag] };
-  }
-
-  Promise.all([
-    req.query.author ? User.findOne({ username: req.query.author }) : null,
-    req.query.favorited
-      ? User.findOne({ username: req.query.favorited })
-      : null,
-  ])
-    .then(function (results) {
-      var author = results[0];
-      var favoriter = results[1];
-
-      if (author) {
-        query.author = author._id;
-      }
-
-      if (favoriter) {
-        query._id = { $in: favoriter.favorites };
-      } else if (req.query.favorited) {
-        query._id = { $in: [] };
-      }
-
-      return Promise.all([
-        Article.find(query)
-          .limit(Number(limit))
-          .skip(Number(offset))
-          .sort({ createdAt: "desc" })
-          .populate("author")
-          .exec(),
-        Article.count(query).exec(),
-        req.payload ? User.findById(req.payload.id) : null,
-      ]).then(function (results) {
-        var articles = results[0];
-        var articlesCount = results[1];
-        var user = results[2];
-
-        return res.json({
-          articles: articles.map(function (article) {
-            return article.toJSONFor(user);
-          }),
-          articlesCount: articlesCount,
-        });
-      });
-    })
-    .catch(next);
+  articleGet(userReq, req, res, next);
 });
 
 router.get("/feed", auth.required, function (req, res, next) {
